f7b613e5614078e4768625758f42a23218b9c46b
Luis <luispsousa90@gmail.com>
2021-05-06 17:15:20 +0100

    Chore: Adding global styles using styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import GlobalStyle from './globalStyle';
 import HomeScreen from './screens/HomeScreen';
 import EpisodeScreen from './screens/EpisodeScreen';
+
+const Wrapper = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+
+  max-width: 960px;
+
+  padding-right: 10px;
+  padding-left: 10px;
+`;
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/episode/:id' component={EpisodeScreen}></Route>
-        <Route path='/' component={HomeScreen}></Route>
-      </Switch>
+      <GlobalStyle />
+      <Wrapper>
+        <Switch>
+          <Route path='/episode/:id' component={EpisodeScreen}></Route>
+          <Route path='/' component={HomeScreen}></Route>
+        </Switch>
+      </Wrapper>
     </Router>
   );
 };
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const EpisodesList = ({ episodes }) => {
+  console.log(episodes);
   return (
     <ul>
-      {episodes.map(({ id, name }) => (
+      {episodes.map(({ id, name, image }) => (
         <li key={name}>
+          {image && (
+            <img src={image.medium} alt={name} width={100} height={50} />
+          )}
           <Link to={`/episode/${id}`}>{name}</Link>
         </li>
       ))}
